87ff0b76bc948191b9653e60d283042d609d50e5
Soundharya Rajendran <94343668+Soundharya2004@users.noreply.github.com>
2023-10-15 21:24:44 +0530

    Delete Portfolio website directory

--- Portfolio website/README.md
+++
@@ -1,3 +0,0 @@
-Sample Portfolio Website
-
-#This project is built using simple HTML, CSS and JS
--- Portfolio website/script.js
+++
@@ -1,5 +0,0 @@
-
-
-function myClickFunction() {
-    window.alert("We will get back to you soon. thank you :)");
-}
